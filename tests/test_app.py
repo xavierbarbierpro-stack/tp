ab03ee1f8690c2e417a7736c8b25c1bbb6a4fbbe
xavierbarbierpro-stack <xavierbarbier.pro@gmail.com>
2026-06-09 15:08:31 +0200

    Add pytest fixture for home route testing
    Added a pytest fixture to test the home route.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 import requests
-
+import pytest
+"""
 def test_app_is_online():
     url = "http://localhost:5000"  # Remplacez par votre URL
 
@@ -14,7 +15,14 @@ def test_app_is_online():
 
     except requests.exceptions.RequestException as e:
         raise AssertionError(f"Impossible de se connecter à l'URL : {str(e)}")
+  """
+
+@pytest.fixture
+def test_home_route(client):
+    """Vérifie que la route principale (/) retourne un statut 200"""
+    response = client.get('/')
+    assert response.status_code == 200, "La route principale (/) ne retourne pas un statut 200."
 
 if __name__ == "__main__":
-    test_app_is_online()
+    #test_app_is_online()
     print("L'application est en ligne et accessible.")
